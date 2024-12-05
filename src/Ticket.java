629c5dd3d8ffe317a4d593706a5be59232fef595
Sukhitha Saparamadu <saparamadusukhitha@gmail.com>
2024-12-05 10:26:51 +0530

    Ticket class

---
+++ src/Ticket.java
@@ -0,0 +1,46 @@
+import java.math.BigDecimal;
+
+public class Ticket {
+    private int ticketid;
+    private String eventName;
+    private BigDecimal ticketPrice;
+
+    public Ticket(int ticketid, String eventName, BigDecimal ticketPrice) {
+        this.ticketid = ticketid;
+        this.eventName = eventName;
+        this.ticketPrice = ticketPrice;
+    }
+
+    public int getTicketid() {
+        return ticketid;
+    }
+
+    public void setTicketid(int ticketid) {
+        this.ticketid = ticketid;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public BigDecimal getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public void setTicketPrice(BigDecimal ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "ticketid=" + ticketid +
+                ", eventName='" + eventName + '\'' +
+                ", ticketPrice=" + ticketPrice +
+                '}';
+    }
+}
