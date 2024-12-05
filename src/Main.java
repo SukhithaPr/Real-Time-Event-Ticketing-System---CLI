99e3dcae4d2842d44f9d33a6c2246239735997ca
Sukhitha Saparamadu <saparamadusukhitha@gmail.com>
2024-12-05 16:59:02 +0530

    Done with the coding

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        
+        TicketPool ticketPool = new TicketPool(10);
+
+        Vendor[] vendors = new Vendor[10];
+        for (int i = 0; i < vendors.length; i++) {
+            vendors[i] = new Vendor(ticketPool, 20, 5);
+            Thread vendorThread = new Thread(vendors[i], "Vender-" + i);
+            vendorThread.start();
+        }
+
+        Customer[] customers = new Customer[10];
+        for (int i = 0; i < customers.length; i++) {
+            customers[i] = new Customer(ticketPool, 5, 5);
+            Thread customerThread = new Thread(customers[i], "Customer-" + i);
+            customerThread.start();
+        }
     }
 }
